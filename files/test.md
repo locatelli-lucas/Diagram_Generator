d08d477064cd7445f5fda2660d0c7fc6fc2d9aca
I755253 <lucas_locatelli2002@hotmail.com>
2025-07-18 15:21:29 -0300

    created entity enums

--- files/test.md
+++ files/test.md
@@ -1,14 +1,172 @@
 erDiagram
-Customer {
-  ID  UUID
-  name  String
-  email  String
-  orders Order
+Category {
+  name String
+  code String
+  status Integer
+  publishStatus PublishStatus
+  parent Category
+  active Boolean
+  categories Category
+  classifications Classification
+  inactiveDate Date
+  description String
+  responsible User
+  approver User
+  segmentationCode Segmentation
+  access Category
+  virtualrestricted Boolean
+  virtualhasChildren Boolean
 }
-Order {
-  ID  UUID
-  orderDate  Date
-  total  Decimal
-  customer Customer
+Classification {
+  category Category
+  domain String
+  name String
+  code String
+  version String
+  classificationUUID String
+  logicalSystem String
+  active Boolean
+  mappedClassification Classification
+  isCustomDomain Boolean
 }
-  Customer }o--o| Order : composition
+RuleEnforcedDomain {
+  domain String
+}
+CategorySequences {
+  type String
+  sequenceNumber Integer
+}
+MaterialGroup {
+  name String
+  code String
+}
+User {
+  userName String
+  lastName String
+  firstName String
+  loginName String
+  displayName String
+  scimExternalId String
+  active Boolean
+  email String
+  emails Email
+  blocked Boolean
+  maxDeletionDate Date
+  userUuid String
+}
+UserBackup {
+  userName String
+  lastName String
+  firstName String
+  loginName String
+  displayName String
+  scimExternalId String
+  active Boolean
+  email String
+  emails Email
+  blocked Boolean
+  maxDeletionDate Date
+  userUuid String
+}
+UserView {
+}
+ActiveUserView {
+}
+CategoryPlanUserView {
+  firstNamelastNameasfullName String
+}
+Email {
+  value String
+  type String
+  isPrimary Boolean
+  user User
+}
+EmailBackup {
+  value String
+  type String
+  isPrimary Boolean
+  user User
+}
+Files {
+  content Binary
+  mediaType String
+  type String
+  fileName String
+  user User
+  username String
+}
+DefaultCategories {
+  user User
+  category Category
+  lastVisitedTime Time
+}
+CategoryPreview {
+  code String
+  name String
+  parentCode String
+  userName String
+  firstName String
+  lastName String
+  approverName String
+  firstApproverName String
+  lastApproverName String
+  isChanged Boolean
+  isUpdated Boolean
+  validType String
+  messages ErrorMessage
+  status Integer
+  fromExcel Integer
+}
+ErrorMessage {
+  message String
+  errorCode Integer
+  errorType Integer
+}
+ClassificationCodeToCategoryResponse {
+  code String
+  category Category
+  typePublishStatus Integer
+  typeSegmentationCode String
+  typeCategoryAccess Integer
+}
+OperationHistory {
+  operationalParam String
+  operation Operation
+  category Category
+  operationalAt Time
+  operator User
+  before String
+  after String
+  typeOperation Integer
+}
+JupiterUserData {
+  displayName String
+  email String
+  firstName String
+  lastName String
+  name String
+  scopes String
+}
+InitialCategoryTree {
+  categoryTree Category
+  allCategories Category
+}
+ReloadCategoryTree {
+  categoryTree Category
+  allCategories Category
+  categoryReloaded Category
+}
+CategoryCoreData {
+  name String
+  code String
+  status Integer
+  parent Category
+  active Boolean
+  categories Category
+  responsible User
+  approver User
+  segmentationCode Segmentation
+  access Category
+  virtualrestricted Boolean
+  virtualhasChildren Boolean
+}
--- utils/Utils.js
+++ utils/Utils.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { primitives, categoryTaxonomyEntities } from '../enums/enums.js';
 
 export const readFile = (filePath) => {
     try {
@@ -22,6 +23,105 @@ export const splitEntities = (data) => {
         .filter(entity => entity.length > 0 && entity.startsWith("entity"));
 }
 
+function parseEntityName(line) {
+    const match = line.match(/entity\s+(\w+)/);
+    return match ? match[1] : null;
+}
+
+function parseAttribute(line) {
+    let name, type;
+    const primitiveTypes = Object.keys(primitives);
+    const projectTypes = Object.keys(categoryTaxonomyEntities);
+    if (line.includes(':') && !line.includes('@')) {
+        let parts = line.split(':');
+        name = parts[0].replace(/key/g, '').replace(/[^a-zA-Z]+/g, '');
+        type = parts[1].replace(/\(.*?\)/g, '').trim();
+
+        const matchedPrimitive = primitiveTypes.find(e => type.includes(e));
+        if (matchedPrimitive) {
+            type = matchedPrimitive;
+        } else {
+            const matchedProjectType = projectTypes.find(e => type.includes(e));
+            type = matchedProjectType || type;
+        }
+
+        if(!type)
+            return null;
+
+    } else {
+        name = line;
+        type = null;
+    }
+    return { name, type };
+}
+
+function handleRelationship(type, entityName, relationships) {
+    const regex = /(?:Composition\s+of\s+(?:one|many)\s+|Association\s+(?:to\s+)?(?:one\s+|many\s+)?)(\w+)/;
+    const match = regex.exec(type);
+    let relationshipType = '';
+    if (!match) return { type, relationshipType: '', match: null };
+
+    if (match[1] !== entityName && relationships.length > 0) {
+        relationships.forEach((relationship) => {
+            if (relationship.primary === match[1] && relationship.secondary === entityName) {
+                if (match[0].includes('of many')) {
+                    relationship.relationshipType += 'o{';
+                } else if (match[0].includes('to many')) {
+                    relationship.relationshipType += '|{';
+                } else if (match[0].includes('to one')) {
+                    relationship.relationshipType += '||';
+                } else {
+                    relationship.relationshipType += 'o|';
+                }
+            }
+        });
+    } else {
+        if (match[0].includes('of many')) {
+            relationshipType += '}o--';
+        } else if (match[0].includes('to many')) {
+            relationshipType += '}|--';
+        } else if (match[0].includes('to one')) {
+            relationshipType += '||--';
+        } else {
+            relationshipType += 'o|--';
+        }
+        relationships.push({
+            primary: entityName,
+            secondary: match ? match[1] : null,
+            connectionType: match[0].startsWith('Association') ? 'association' : 'composition',
+            relationshipType: relationshipType
+        });
+    }
+    return { type: match[1], relationshipType, match };
+}
+
+function processEntity(e, stream, relationships, entitiesNames) {
+    const lines = e.split(/\r?\n/);
+    const entityName = parseEntityName(lines[0]);
+    if (!entityName) return;
+    stream.write(entityName + ' {\n');
+
+    if (!entitiesNames.includes(entityName))
+        entitiesNames.push(entityName);
+
+    for (let i = 1; i < lines.length; i++) {
+        let line = lines[i].replace(/;/g, '');
+        if (!line.trim()) continue;
+        let { name, type } = parseAttribute(line);
+        if(!name || !type) continue;
+
+        if (type && (type.includes('Association') || type.includes('Composition'))) {
+            const relResult = handleRelationship(type, entityName, relationships);
+            type = relResult.type;
+        } else if (!type) {
+            stream.write(`${line}\n`);
+            continue;
+        }
+        stream.write(`  ${name} ${type}\n`);
+    }
+    stream.write('}\n');
+}
+
 export const writeFile = (output, input) => {
     fs.writeFileSync(output, '', { flag: 'w' });
     const data = readFile(input);
@@ -30,74 +130,13 @@ export const writeFile = (output, input) => {
     let relationships = [];
     let entitiesNames = [];
 
-    stream.write('erDiagram\n')
+    stream.write('erDiagram\n');
 
     entities.forEach((e) => {
-        const lines = e.split(/\r?\n/);
-        const entityName = lines[0].match(/entity\s+(\w+)/);
-        stream.write(entityName[1] + ' {\n');
-
-        if (!entitiesNames.includes(entityName[1]))
-            entitiesNames.push(entityName[1]);
-
-        for (let i = 1; i < lines.length; i++) {
-            let line = lines[i].replace(/;/g, '');
-            let type;
-            let name;
-
-            if (line.includes(':')) {
-                line = line.split(':')
-                name = line[0].replace(/key/g, '').replace(/[^a-zA-Z]+/g, '');
-                type = line[1].replace(/\(.*?\)/g, '');
-                if (type.includes('Association') || type.includes('Composition')) {
-                    const regex = /(?:Composition\s+of\s+(?:one|many)\s+|Association\s+(?:to\s+)?(?:one\s+|many\s+)?)(\w+)/;
-                    const match = regex.exec(type);
-                    let relationshipType = '';
-
-                    if (match[1] != entityName[1] && relationships.length > 0) {
-                        relationships.forEach((relationship) => {
-                            if (relationship.primary === match[1] && relationship.secondary === entityName[1]) {
-                                if (match[0].includes('of many')) {
-                                    relationship.relationshipType += 'o{'
-                                } else if (match[0].includes('to many'))
-                                    relationship.relationshipType += '|{';
-                                else if (match[0].includes('to one')) {
-                                    relationship.relationshipType += '||';
-                                } else {
-                                    relationship.relationshipType += 'o|';
-                                }
-                            }
-                        })
-                    } else {
-                        if (match[0].includes('of many')) {
-                            relationshipType += '}o--'
-                        } else if (match[0].includes('to many'))
-                            relationshipType += '}|--';
-                        else if (match[0].includes('to one')) {
-                            relationshipType += '||--';
-                        } else {
-                            relationshipType += 'o|--';
-                        }
-                        relationships.push(
-                            {
-                                primary: entityName[1],
-                                secondary: match ? match[1] : null,
-                                connectionType: match[0].startsWith('Association') ? 'association' : 'composition',
-                                relationshipType: relationshipType
-                            }
-                        );
-                    }
-                    type = match[1];
-                }
-            } else {
-                stream.write(`${line}\n`);
-                continue;
-            }
-            stream.write(`  ${name} ${type}\n`);
-        }
-
+        processEntity(e, stream, relationships, entitiesNames);
     });
+
     relationships.forEach(e => {
         stream.write(`  ${e.primary} ${e.relationshipType} ${e.secondary} : ${e.connectionType}\n`);
-    })
+    });
 }
